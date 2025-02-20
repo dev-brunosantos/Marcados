e442c35f7eccd5fa5e3cdd50eb7d2774db58b4a4
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-02-20 13:10:18 -0300

    Criação do arquivo que armazenará todos os contextos da aplicação

---
+++ src/contexts/AppContexts.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from "react"
+import { LoginProvider } from "./LoginContext"
+
+export const AppContextProvider = ({ children }: { children: ReactNode }) => {
+    return (
+        <LoginProvider>
+            { children }
+        </LoginProvider>
+    )
+}
