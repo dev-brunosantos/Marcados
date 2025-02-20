697b065a2d935339df58e1f6ebe653f34394ba49
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-02-20 14:17:11 -0300

    Finalizando o contexto de cadastro do usuário

--- src/contexts/UsuarioCadastroContext.tsx
+++ src/contexts/UsuarioCadastroContext.tsx
@@ -1,4 +1,6 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
+import { useRouter } from 'next/navigation'
+import { ReactProps } from "../interface/IReactProps";
 
 interface Usuario {
     nome: string;
@@ -9,9 +11,51 @@ interface Usuario {
 
 interface UsuarioCadastro {
     usuario: Usuario;
-    cadastro: (nome: string, email: string, senha: string, sobrenome?:string) => void;
+    cadastroUsuario: (
+        nome: string, email: string, senha:
+            string, confirmarSenha: string, sobrenome?: string
+    ) => void;
     erro: boolean;
 }
 
 
-const UsuarioCadastroContext = createContext<undefined>(undefined)
+const UsuarioCadastroContext = createContext<UsuarioCadastro | undefined>(undefined)
+
+const UsuarioCadastroProvider = ({ children }: ReactProps) => {
+
+    const router = useRouter()
+
+    const [usuario, setUsuario] = useState<Usuario>({
+        nome: '', sobrenome: '',
+        email: '', senha: ''
+    })
+
+    const [erro, setErro] = useState(false)
+
+    function cadastroUsuario(nome: string, email: string, senha: string, confirmarSenha: string, sobrenome?: string) {
+        if (nome.trim() === "" || email.trim() === "" || senha.trim() === "") {
+            setErro(true)
+            return alert("Todos os campos devem ser preenchidos para a realização do Login.")
+        }
+
+        if (confirmarSenha !== senha) {
+            setErro(true)
+            return alert("As senhas não são iguais, por favor verifque.")
+        }
+
+        setErro(false)
+        
+        return(
+            alert("Usuário cadastrado com sucesso"),
+            router.push('/login')
+        )
+    }
+
+    return (
+        <UsuarioCadastroContext.Provider value={{ usuario, cadastroUsuario, erro }}>
+            {children}
+        </UsuarioCadastroContext.Provider>
+    )
+}
+
+export { UsuarioCadastroContext, UsuarioCadastroProvider }
