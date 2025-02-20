3cfd02c19d00afb800f9ff5cce8c87c979046cb0
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-02-20 13:22:00 -0300

    Adicionando as funcionalidades do contexto de Login

--- src/app/login/index.tsx
+++ src/app/login/index.tsx
@@ -1,7 +1,19 @@
 import { BtnComponent } from "@/src/components/Btn";
 import { InputComponent } from "@/src/components/Input";
+import { useLogin } from "@/src/hooks/useLogin";
+import { useState } from "react";
 
 export default function Login() {
+
+    const funcLogin = useLogin();
+
+    const [email, setemail] = useState('')
+    const [senha, setSenha] = useState('')
+
+    function realizarLogin() {
+        funcLogin?.login(email, senha)
+    }
+
     return (
         <div>
 
@@ -9,21 +21,31 @@ export default function Login() {
                 <InputComponent
                     isPassword={false}
                     placeholder="Digite seu e-mail"
+                    onChange={e => setemail(e.target.value)}
                 />
                 <InputComponent
                     isPassword={false}
                     placeholder="Digite sua senha"
+                    onChange={e => setSenha(e.target.value)}
                 />
             </div>
 
             <div>
-                <BtnComponent 
+                <BtnComponent
                     titulo="Entrar"
+                    onClick={realizarLogin}
                 />
             </div>
 
             <div>
-                
+                {
+                    funcLogin?.erro && (
+                        <>
+                            <p>Não tem conta?</p>
+                            <a href="">Clique aqui e cadastre-se</a>
+                        </>
+                    )
+                }
             </div>
         </div>
     )
