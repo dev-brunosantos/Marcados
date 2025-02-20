194617ce91dcc90af34c590711483e63da1cb53e
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-02-20 17:58:59 -0300

    Melhorando o botão de menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,32 +1,41 @@
 "use client"
 
 import Link from "next/link"
-import { useRouter } from 'next/navigation';
-import { Undo2 } from 'lucide-react';
+import { Menu } from 'lucide-react';
+import { useState } from "react";
 
 export const MenuPrincipal = () => {
+    const [classeMenu, setClasseMenu] = useState('fechar-menu');
 
-    const router = useRouter();
+    function mostrarMenu() {
+        classeMenu === 'fechar-menu' ? setClasseMenu('abrir-menu') : setClasseMenu('fechar-menu')
+    }
 
-    return(
-        <header>
+    return (
+        <header className="
+          w-full h-14 border px-4 flex items-center justify-between fixed top-0 left-0 
+        ">
             <div>
-                <a href="">Logo</a>
+                <a href="">Marcados</a>
             </div>
 
             <nav>
-                <ul>
+                <div className="h-full md:hidden">
+                    <button className="h-full flex items-center justify-center" onClick={mostrarMenu}>
+                        <Menu />
+                    </button>
+                </div>
+
+                <ul
+                    className={`
+                      ${classeMenu} px-4 flex flex-col justify-items-end justify-evenly absolute right-1 top-[100%]
+                    `}
+                >
                     <Link href=''>Home</Link>
                     <Link href=''>Escalas</Link>
                     <Link href=''>Louvores</Link>
+                    <Link href='/login'>Sair</Link>
                 </ul>
-
-                <button onClick={() => router.push('/login')}>
-                    Sair 
-                    <span>
-                        <Undo2 />
-                    </span>
-                </button>
             </nav>
         </header>
     )
