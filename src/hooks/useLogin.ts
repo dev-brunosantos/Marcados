b98eaaff68d802946ff1a903a60a1a95499c9ea4
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-02-20 13:06:37 -0300

    Criando o hook para o uso do contexto Login

---
+++ src/hooks/useLogin.ts
@@ -0,0 +1,14 @@
+import { useContext, ContextType } from "react"
+import { LoginContext } from "../contexts/LoginContext"
+
+interface UseLogin {
+    (): ContextType<typeof LoginContext>;
+}
+
+export const useLogin: UseLogin = () => {
+    const contexto = useContext(LoginContext)
+    if(!contexto) {
+        throw new Error("Não existe nenhum contexto de LOGIN no sistema.")
+    }
+    return contexto
+}
