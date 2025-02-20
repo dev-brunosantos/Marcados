abff922e7f45316b2fe2535952580583d21bb05f
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-02-20 14:07:04 -0300

    Criando a pasta que será responsável por armazenar os códigos referente a cadastros

---
+++ src/app/cadastro/usuario/page.tsx
@@ -0,0 +1,69 @@
+"use client"
+
+import { useState } from "react";
+import { BtnComponent } from "@/src/components/Btn";
+import { InputComponent } from "@/src/components/Input";
+import { useLogin } from "@/src/hooks/useLogin";
+
+export default function NovoUsuario() {
+
+    const { login, erro } = useLogin();
+
+    const [email, setemail] = useState('')
+    const [senha, setSenha] = useState('')
+
+    function realizarLogin() {
+        login(email, senha)
+    }
+
+    return (
+        <div>
+
+            <div>
+                <InputComponent
+                    isPassword={false}
+                    placeholder="Digite seu nome"
+                    onChange={e => setemail(e.target.value)}
+                />
+                <InputComponent
+                    isPassword={false}
+                    placeholder="Digite seu sobrenome"
+                    onChange={e => setemail(e.target.value)}
+                />
+                <InputComponent
+                    isPassword={false}
+                    placeholder="Digite seu e-mail"
+                    onChange={e => setemail(e.target.value)}
+                />
+                <InputComponent
+                    isPassword={true}
+                    placeholder="Digite sua senha"
+                    onChange={e => setSenha(e.target.value)}
+                />
+                <InputComponent
+                    isPassword={true}
+                    placeholder="Confirme sua senha"
+                    onChange={e => setSenha(e.target.value)}
+                />
+            </div>
+
+            <div>
+                <BtnComponent
+                    titulo="Cadastrar"
+                    onClick={realizarLogin}
+                />
+            </div>
+
+            <div>
+                {
+                    erro && (
+                        <>
+                            <p>Não tem conta?</p>
+                            <a href="/cadastro/usuario">Clique aqui e cadastre-se</a>
+                        </>
+                    )
+                }
+            </div>
+        </div>
+    )
+}
