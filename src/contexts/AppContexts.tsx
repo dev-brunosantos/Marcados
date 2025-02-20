fc8684a3cc382ff024e7940dfc46f0f8a8d4a895
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-02-20 14:19:44 -0300

    Adicionando o contexto no arquivo principal de contexto

--- src/contexts/AppContexts.tsx
+++ src/contexts/AppContexts.tsx
@@ -1,10 +1,13 @@
 import { ReactNode } from "react"
 import { LoginProvider } from "./LoginContext"
+import { UsuarioCadastroProvider } from "./UsuarioCadastroContext"
 
 export const AppContextProvider = ({ children }: { children: ReactNode }) => {
     return (
         <LoginProvider>
-            { children }
+            <UsuarioCadastroProvider>
+                {children}
+            </UsuarioCadastroProvider>
         </LoginProvider>
     )
 }
