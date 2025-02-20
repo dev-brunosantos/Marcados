a91adcfe9f9771a089f7f14ccfaf382035679070
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-02-20 13:28:58 -0300

    Adicionando o 'use client' para o arquivo funcionar do lado do cliente

---
+++ src/app/login/page.tsx
@@ -0,0 +1,54 @@
+"use client"
+
+import { BtnComponent } from "@/src/components/Btn";
+import { InputComponent } from "@/src/components/Input";
+import { useLogin } from "@/src/hooks/useLogin";
+import { useState } from "react";
+
+export default function Login() {
+
+    const funcLogin = useLogin();
+
+    const [email, setemail] = useState('')
+    const [senha, setSenha] = useState('')
+
+    function realizarLogin() {
+        funcLogin?.login(email, senha)
+    }
+
+    return (
+        <div>
+
+            <div>
+                <InputComponent
+                    isPassword={false}
+                    placeholder="Digite seu e-mail"
+                    onChange={e => setemail(e.target.value)}
+                />
+                <InputComponent
+                    isPassword={false}
+                    placeholder="Digite sua senha"
+                    onChange={e => setSenha(e.target.value)}
+                />
+            </div>
+
+            <div>
+                <BtnComponent
+                    titulo="Entrar"
+                    onClick={realizarLogin}
+                />
+            </div>
+
+            <div>
+                {
+                    funcLogin?.erro && (
+                        <>
+                            <p>Não tem conta?</p>
+                            <a href="">Clique aqui e cadastre-se</a>
+                        </>
+                    )
+                }
+            </div>
+        </div>
+    )
+}
