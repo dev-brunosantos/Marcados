5d55299484be87d5f07d671c61d6a59d66cccfa7
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-02-20 17:10:05 -0300

    Removendo a cor teste do componente Tela

--- src/app/_components/Tela.tsx
+++ src/app/_components/Tela.tsx
@@ -2,7 +2,7 @@ import { ReactProps } from "@/src/interface/IReactProps";
 
 export function Tela({ children }: ReactProps) {
     return (
-        <div className="w-full h-[100vh] bg-red-400 flex flex-col items-center justify-evenly">
+        <div className="w-full h-[100vh] flex flex-col items-center justify-evenly">
             {children}
         </div>
     )
