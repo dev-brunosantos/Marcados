75995895e6ebe04f56dd470ccb61f783e94d778e
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-02-24 17:06:02 -0300

    Iniciando a criação do componente de input personalizado

---
+++ src/components/Input.tsx
@@ -0,0 +1,45 @@
+import { InputHTMLAttributes, useState } from 'react'
+import { Eye, EyeClosed } from 'lucide-react';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+    isPassword: boolean;
+}
+
+export const InputComponent = ({ isPassword, ...rest }: InputProps) => {
+
+    const [tipo, setTipo] = useState("password")
+
+    function verSenha() {
+        tipo === "password" ? setTipo("text") : setTipo("password")
+    }
+
+    if (!isPassword) {
+        return (
+            <div>
+                <input
+                    type="text"
+                    {...rest}
+                />
+            </div>
+        )
+    }
+
+    return (
+        <div>
+            <input
+                type={tipo}
+                {...rest}
+            />
+
+            <button
+                onClick={verSenha}
+            >
+                {tipo === "password" ?
+                    <EyeClosed /> :
+                    <Eye />
+                }
+
+            </button>
+        </div>
+    )
+}
