f6d966662d7b46d48fabcd213c00a6bafc50fb17
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-02-24 18:04:10 -0300

    Criando o hook de cadastro do usuário

--- src/hooks/useCadastroUsuario.ts
+++ src/hooks/useCadastroUsuario.ts
@@ -1,5 +1,10 @@
 import { useContext } from "react"
+import { UsuarioCadastroContext } from "../contexts/UsuarioCadastroContext"
 
 export const useCadastroUsuario = () => {
-    const contexto = useContext()
+    const contexto = useContext(UsuarioCadastroContext)
+    if(!contexto) {
+        throw new Error("Não existe nenhum contexto de Cadastro de Usuário.")
+    }
+    return contexto
 }
